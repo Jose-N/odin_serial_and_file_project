d37239e9950c74a03d06716b5196409fe1594500
Jose Naylor <jose.c.naylor@gmail.com>
2017-05-03 02:09:36 +0000

    program can now read all the first names, and first names only, off of the csv file

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -1,4 +1,9 @@
 puts "EventManager Initialized"
 
-contents = File.read "event_attendees.csv"
-puts contents
+lines = File.readlines "event_attendees.csv"
+lines.each_with_index do |line,index|
+  next if index == 0
+  colums = line.split(",")
+  name = colums[2]
+  puts name
+end
