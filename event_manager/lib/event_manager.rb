d154ca92ebe16b4864f7b78d987aa0adc6938754
Jose Naylor <jose.c.naylor@gmail.com>
2017-05-03 02:35:56 +0000

    redid program using ruby's csv library

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -1,9 +1,24 @@
-puts "EventManager Initialized"
+require "csv"
 
-lines = File.readlines "event_attendees.csv"
-lines.each_with_index do |line,index|
-  next if index == 0
-  colums = line.split(",")
-  name = colums[2]
-  puts name
+def clean_zipcode(zipcode)
+  if zipcode.nil?
+    "00000"
+  elsif zipcode.length < 5
+    zipcode.rjust 5, "0"
+  elsif zipcode.length > 5
+    zipcode[0..4]
+  else
+    zipcode
+  end
+end
+
+puts "EventManager initialized."
+
+contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+contents.each do |row|
+  name = row[:first_name]
+
+  zipcode = clean_zipcode(row[:zipcode])
+
+  puts "#{name} #{zipcode}"
 end
