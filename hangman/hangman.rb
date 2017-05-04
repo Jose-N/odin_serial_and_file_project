34e4015518e8b6681dff976b1a0a55774b9e1db7
Jose Naylor <jose.c.naylor@gmail.com>
2017-05-04 05:14:08 +0000

    created a console hangman game, need to add ability to save and load. Maybe add hangman picture instead of # of tries left

--- hangman/hangman.rb
+++ hangman/hangman.rb
@@ -1,8 +1,51 @@
-# generate hangman image in console
+class Hangman
+  def initialize()
+    @word = ""
+    @hangman = ""
+  end
 
-# Ask to load
+  def newWord
+    lines = File.readlines "5desk.txt"
+    randLine = rand(0...61406)
+    lines.each_with_index do |line,index|
+      if index == randLine && line.length < 12 && line.length >5
+        @word = line.chomp.downcase.to_s
+      end
+    end
+  end
 
-# No load? Generate new word 5 - 12 char from dictionary file
+  def play
+    hiddenWord = "" 
+    wrongLetters = "" 
+    triesLeft = 6
+    @word.length.times{ hiddenWord << "*" }
+    until triesLeft == 0 || hiddenWord == @word
+      puts "You have " + triesLeft.to_s + " tries left!"
+      #puts "Word:" + @word
+      #puts @word.length.to_s
+      puts "Hidden word: " + hiddenWord
+      puts "Wrong Letters: " + wrongLetters
+      puts "Please guess a letter:"  
+      guess = gets.chomp.downcase.to_s  
+      if @word.include? guess
+        @word.each_char.with_index do |letter, index|
+          if letter == guess
+            hiddenWord[index] = guess
+          end
+        end
+      else 
+        triesLeft -= 1
+        wrongLetters << guess + " "
+      end
+    end
+    if triesLeft == 0
+      puts "You Lose!"
+    elsif hiddenWord == @word
+      puts "You Win!"
+    end
+  end
 
-# logic to guess word
-# start of turn can save game 
+end
+hangman = Hangman.new
+hangman.newWord
+hangman.play
