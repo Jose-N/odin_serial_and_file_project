0ed14f6763c5778ead4a410c09a3a838c3706604
Jose Naylor <jose.c.naylor@gmail.com>
2017-05-07 14:04:59 +0000

    add ability to save and load game but loading game results in insta win

--- hangman/hangman.rb
+++ hangman/hangman.rb
@@ -1,3 +1,6 @@
+require 'date'
+require 'yaml'
+
 class Hangman
   def initialize()
     @word = ""
@@ -12,40 +15,90 @@ class Hangman
     lines.each_with_index do |line,index|
       if index == randLine && line.length < 12 && line.length >5
         @word = line.chomp.downcase.to_s
+        @word.length.times{ @hiddenWord << "*" }
       end
+    end
+  end
+
+  def didWin
+    win = false
+    if @triesLeft == 0
+      puts "You Lose!"
+      puts "The correct word was: " + @word
+      return win = true
+    elsif @hiddenWord == @word
+      puts "You Win!"
+      return win = true
     end
   end
 
   def play
-    @word.length.times{ @hiddenWord << "*" }
-    until @triesLeft == 0 || @hiddenWord == @word
-      puts "You have " + @triesLeft.to_s + " tries left!"
-      #puts "Word:" + @word
-      #puts @word.length.to_s
-      puts "Hidden word: " + @hiddenWord
-      puts "Wrong Letters: " + @wrongLetters
-      puts "Please guess a letter:"  
-      guess = gets.chomp.downcase.to_s  
-      if @word.include? guess
-        @word.each_char.with_index do |letter, index|
-          if letter == guess
-            @hiddenWord[index] = guess
-          end
+    puts "You have " + @triesLeft.to_s + " tries left!"
+    #puts "Word:" + @word
+    #puts @word.length.to_s
+    puts "Hidden word: " + @hiddenWord
+    puts "Wrong Letters: " + @wrongLetters
+    puts "Please guess a letter:"  
+    guess = gets.chomp.downcase.to_s  
+    if @word.include? guess
+      @word.each_char.with_index do |letter, index|
+        if letter == guess
+          @hiddenWord[index] = guess
         end
-      else 
-        @triesLeft -= 1
-        @wrongLetters << guess + " "
       end
-    end
-    if @triesLeft == 0
-      puts "You Lose!"
-      puts "The correct word was: " + @word
-    elsif @hiddenWord == @word
-      puts "You Win!"
+    else 
+      @triesLeft -= 1
+      @wrongLetters << guess + " "
     end
   end
-
 end
+
 hangman = Hangman.new
-hangman.newWord
-hangman.play
+puts "Would you like to load a game: Yes/No?"
+response = gets.chomp.downcase.to_s
+if response == "yes"
+  listGames = Dir.glob("saves/*")
+  puts listGames
+  puts "What is the save file you want to open"
+  saveFile = gets.chomp.to_s
+  saveFileFull = "saves/" + saveFile
+  oldGame = YAML.load(File.read(saveFileFull))
+  until hangman.didWin
+    hangman.play
+    puts "Would you like to save your game: Yes/No?"
+    save = gets.chomp.downcase.to_s
+    if save == "yes"
+      if Dir.exist?('saves')
+        fileName = "saves/savegame" + DateTime.now.to_s + ".txt"
+        File.open(fileName, 'w') { |f| f.write(YAML.dump(hangman))}
+        puts "Your game has been saved"
+      else
+        Dir.mkdir "saves"
+        puts "Made a saves folder"
+        fileName = "saves/savegame" + DateTime.now.to_s + ".txt"
+        File.open(fileName, 'w') { |f| f.write(YAML.dump(hangman))}
+        puts "Your game has been saved"
+      end
+    end
+  end
+else
+  hangman.newWord
+  until hangman.didWin
+    hangman.play
+    puts "Would you like to save your game: Yes/No?"
+    save = gets.chomp.downcase.to_s
+    if save == "yes"
+      if Dir.exist?('saves')
+        fileName = "saves/savegame" + DateTime.now.to_s + ".txt"
+        File.open(fileName, 'w') { |f| f.write(YAML.dump(hangman))}
+        puts "Your game has been saved"
+      else
+        Dir.mkdir "saves"
+        puts "Made a saves folder"
+        fileName = "saves/savegame" + DateTime.now.to_s + ".txt"
+        File.open(fileName, 'w') { |f| f.write(YAML.dump(hangman))}
+        puts "Your game has been saved"
+      end
+    end
+  end
+end
